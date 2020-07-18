bb28021de555116c28cce26992bf34200e2f23cd
cyberkurumin <jorge@mylittleparis.com>
2020-07-18 23:54:51 +0200

    WIP - Adding files for list and summary

---
+++ components/account/postsList.js
@@ -0,0 +1,7 @@
+export default function PostList() {
+    return (
+        <div>
+            <h2> The Results Table</h2>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,46 @@
 import Head from 'next/head'
 import { useFormik } from 'formik'
+import Summary from '../components/account/summary'
+import PostList from '../components/account/postsList'
 
-export default function Home() {
+
+const validate = values => {
+    const errors = {}
+    if (!values.profile) {
+        errors.profile = 'This is a required field'
+    }
+
+    return errors
+}
+
+const Home = () => {
+
   const formik = useFormik({
       initialValues: {
           profile: ''
       },
+      validate,
       onSubmit: values => {
-          console.log(values)
+          console.log(values.profile)
       }
   })
 
   return (
-      <form onSubmit={ formik.handleSubmit }>
-        <div>
-            <label htmlFor='profile'>Instagram Profile</label><br />
-            <input type='text' name='profile' onChange={ formik.handleChange } value={ formik.values.profile } />
-        </div>
-        <div>
-            <button type='submit' >Launch Research</button>
-        </div>
-      </form>
+      <div>
+          <form onSubmit={ formik.handleSubmit }>
+            <div>
+                {formik.errors.profile ? <div>{formik.errors.profile}</div> : null}
+                <label htmlFor='profile'>Instagram Profile</label><br />
+                <input type='text' name='profile' onChange={ formik.handleChange } value={ formik.values.profile } />
+            </div>
+            <div>
+                <button type='submit' >Launch Research</button>
+            </div>
+          </form>
+          <Summary></Summary>
+          <PostList></PostList>
+      </div>
   )
 }
+
+export default Home;
